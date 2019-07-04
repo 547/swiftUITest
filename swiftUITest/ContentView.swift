a3ebab12612314ef0a5e0a3a7692b38444d40111
Seven Wang <seven.wang@supernovachina.com>
2019-07-04 13:02:57 +0800

    Initial Commit

---
+++ swiftUITest/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  swiftUITest
+//
+//  Created by Supernova SanDick SSD on 2019/7/4.
+//  Copyright © 2019 Seven. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
