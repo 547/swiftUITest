396ed7a17b1d330c0eefb3d517441f91e77468a6
Seven Wang <seven.wang@supernovachina.com>
2019-07-15 12:03:37 +0800

    update

---
+++ swiftUITest/Views/NavigationTestView.swift
@@ -0,0 +1,23 @@
+//
+//  NavigationTestView.swift
+//  swiftUITest
+//
+//  Created by Supernova SanDick SSD on 2019/7/15.
+//  Copyright © 2019 Seven. All rights reserved.
+//
+
+import SwiftUI
+
+struct NavigationTestView : View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct NavigationTestView_Previews : PreviewProvider {
+    static var previews: some View {
+        NavigationTestView()
+    }
+}
+#endif
