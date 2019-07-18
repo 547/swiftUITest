2289d558d223720dde7b90495a5ba680aa0135f8
Seven Wang <seven.wang@supernovachina.com>
2019-07-18 15:40:30 +0800

    update

--- swiftUITest/Views/NavigationTestView.swift
+++ swiftUITest/Views/NavigationTestView.swift
@@ -14,14 +14,21 @@ struct NavigationTestView : View {
     var body: some View {
         //目前SwiftUI好像还没有方法可以设置navigationbar背景颜色和title字体颜色的，可能以后会更新吧
         NavigationView {
-            List.init {
-                ForEach.init(0...8) { _ in
-                    Text("45545")
-                }
+            VStack{
+                Rectangle.init()
+                    .foregroundColor(.red)
+                    .frame(width: UIScreen.main.bounds.width, height: 160).edgesIgnoringSafeArea(.top)
+              
+                List.init {
+                    ForEach.init(0...8) { _ in
+                        Text("45545")
+                    }
+                }.background(Color.red).offset(y: -160)
+                
             }
+                
             .navigationBarTitle("title", displayMode: .large)
         }
-        
     }
 }
 
